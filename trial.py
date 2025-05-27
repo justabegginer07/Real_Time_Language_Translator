613d73c431a46d2c9fa7265e210747d86a58191f
Poongodhai <78218998+justabegginer07@users.noreply.github.com>
2025-05-27 17:29:01 +0000

    trial file update

--- trial.py
+++ trial.py
@@ -2,17 +2,14 @@ import speech_recognition as sr
 import pyttsx3
 import whisper
 from ffmpeg import *
+import os
 
+os.environ['PATH']+=os.pathsep+os.path.abspath('.')
 
 #print(whisper.available_models())
 model=whisper.load_model('base')
-try:
-    transcription=model.transcribe('harvard.wav',fp16=False)
-
-except:
-    ffmpeg_download()
-    transcription=model.transcribe('harvard.wav')
-
+ffmpeg_download()
+transcription=model.transcribe('harvard.wav',fp16=False)
 print(transcription['text'])
 
 
